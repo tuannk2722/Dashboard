fb3726c0e78edd04a5c473f2953a579124d8998c
tuannk1 <tuannk1@hblab.vn>
2026-03-31 15:18:28 +0700

    fix export handler

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,11 @@
-import { authConfig } from '@/auth.config';
-import NextAuth from 'next-auth';
+// import { authConfig } from '@/auth.config';
+// import NextAuth from 'next-auth';
 
-const handler = NextAuth(authConfig);
+// const handler = NextAuth(authConfig);
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
+
+import { auth } from '@/auth';
+
+export const GET = auth;
+export const POST = auth;
