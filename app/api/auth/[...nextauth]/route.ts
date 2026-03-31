094dc3c2fe2489ac1be7d41b47f0afdedb22bcc0
tuannk1 <tuannk1@hblab.vn>
2026-03-31 15:27:51 +0700

    fix auth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,11 @@
 // import { authConfig } from '@/auth.config';
 // import NextAuth from 'next-auth';
 
+
 // const handler = NextAuth(authConfig);
 
 // export { handler as GET, handler as POST };
 
-import { auth } from '@/auth';
+import { handlers } from "@/auth";
 
-export const GET = auth;
-export const POST = auth;
+export const { GET, POST } = handlers;
